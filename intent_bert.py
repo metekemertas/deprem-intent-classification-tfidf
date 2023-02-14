96a2199fd71aba6c0d1bf25b8382229c9cc2dfec
Mete Kemertas <metekemertas@gmail.com>
2023-02-13 20:53:25 -0500

    LLRD implementation added

--- intent_bert.py
+++ intent_bert.py
@@ -11,8 +11,9 @@ from huggingface_hub import login
 from sklearn.metrics import classification_report, f1_score
 from sklearn.model_selection import train_test_split
 from unicode_tr import unicode_tr
-from transformers import (AutoTokenizer, AutoModelForSequenceClassification,
-                          Trainer, TrainingArguments, EarlyStoppingCallback)
+from transformers import (AdamW, AutoConfig, AutoTokenizer, AutoModelForSequenceClassification,
+                          Trainer, TrainingArguments, EarlyStoppingCallback,
+                          get_cosine_schedule_with_warmup, get_linear_schedule_with_warmup)
 
 LABEL_NAMES = [
     'Alakasiz', 'Barinma', 'Elektronik',
@@ -20,31 +21,61 @@ LABEL_NAMES = [
     'Su', 'Yagma', 'Yemek']
 
 
-class IntentDataset(th.utils.data.Dataset):
+def get_optimizer_grouped_parameters(
+    model, model_type,
+    learning_rate, weight_decay,
+    layerwise_learning_rate_decay
+):
+    no_decay = ["bias", "LayerNorm.weight"]
+    # initialize lr for task specific layer
+    optimizer_grouped_parameters = [
+        {
+            "params": [p for n, p in model.named_parameters() if "classifier" in n or "pooler" in n],
+            "weight_decay": 0.0,
+            "lr": learning_rate,
+        },
+    ]
+    # initialize lrs for every layer
+    num_layers = model.config.num_hidden_layers
+    layers = [getattr(model, model_type).embeddings] + list(getattr(model, model_type).encoder.layer)
+    layers.reverse()
+    lr = learning_rate
+    for layer in layers:
+        lr *= layerwise_learning_rate_decay
+        optimizer_grouped_parameters += [
+            {
+                "params": [p for n, p in layer.named_parameters() if not any(nd in n for nd in no_decay)],
+                "weight_decay": weight_decay,
+                "lr": lr,
+            },
+            {
+                "params": [p for n, p in layer.named_parameters() if any(nd in n for nd in no_decay)],
+                "weight_decay": 0.0,
+                "lr": lr,
+            },
+        ]
+    return optimizer_grouped_parameters
 
-    def __init__(self, df, tokenizer, num_classes=-1, label_col="labels", text_col="text_cleaned"):
-        self.df = df
-        self.tokenizer = tokenizer
-        self.num_classes = num_classes
-        self.label_col = label_col
-        self.text_col = text_col
 
-    def __len__(self):
-        return len(self.df)
+def get_llrd_optimizer_scheduler(model, learning_rate=1e-5, weight_decay=0.01, layerwise_learning_rate_decay=0.95):
+    grouped_optimizer_params = get_optimizer_grouped_parameters(
+        model, 'bert',
+        learning_rate, weight_decay,
+        layerwise_learning_rate_decay
+    )
+    optimizer = AdamW(
+        grouped_optimizer_params,
+        lr=learning_rate,
+        eps=1e-6,
+        correct_bias=True
+    )
+    scheduler = get_cosine_schedule_with_warmup(
+        optimizer,
+        num_warmup_steps=0,
+        num_training_steps=15
+    )
 
-    def __getitem__(self, idx):
-        row = self.df.iloc[idx]
-        text, label = row.text, self._encode_label(row[self.label_col])
-        encoding = self.tokenizer(text, max_length=64, padding="max_length", truncation=True)
-        encoding = {key: th.tensor(val) for key, val in encoding.items()}
-        encoding[self.label_col] = th.tensor(label)
-        return dict(encoding)
-
-    def _encode_label(self, labels):
-        encoded_labels = np.zeros(self.num_classes)
-        for label in labels:
-            encoded_labels[label] = 1.0
-        return encoded_labels
+    return optimizer, scheduler
 
 
 def prep_datasets(tokenizer,
@@ -113,10 +144,37 @@ def compute_f1(eval_pred):
     return {"f1": f1_score(predictions, labels, average="macro")}
 
 
+class IntentDataset(th.utils.data.Dataset):
+
+    def __init__(self, df, tokenizer, num_classes=-1, label_col="labels", text_col="text_cleaned"):
+        self.df = df
+        self.tokenizer = tokenizer
+        self.num_classes = num_classes
+        self.label_col = label_col
+        self.text_col = text_col
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        text, label = row.text, self._encode_label(row[self.label_col])
+        encoding = self.tokenizer(text, max_length=64, padding="max_length", truncation=True)
+        encoding = {key: th.tensor(val) for key, val in encoding.items()}
+        encoding[self.label_col] = th.tensor(label)
+        return dict(encoding)
+
+    def _encode_label(self, labels):
+        encoded_labels = np.zeros(self.num_classes)
+        for label in labels:
+            encoded_labels[label] = 1.0
+        return encoded_labels
+
+
 def main():
-    login(token='<your-HF-login-token>')
-    # model_name = "dbmdz/bert-base-turkish-uncased"
-    model_name = "<path-to-BERT-finetuned-on-unlabelled-tweets>"
+    login(token='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')
+    model_name = "dbmdz/bert-base-turkish-uncased"
+    # model_name = "<path-to-BERT-finetuned-on-unlabelled-tweets>"
     tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-base-turkish-uncased")
     train_ds, val_ds, test_ds = prep_datasets(tokenizer)
 
@@ -129,20 +187,26 @@ def main():
         save_strategy="epoch",
         per_device_train_batch_size=32,
         per_device_eval_batch_size=32,
-        weight_decay=0.01,
+        # weight_decay=0.01,
         report_to=None,
         num_train_epochs=15,
         metric_for_best_model="f1",
         load_best_model_at_end=True,
     )
 
+    optimizer, scheduler = get_llrd_optimizer_scheduler(
+        model,
+        learning_rate=5e-5,
+        weight_decay=0.01,
+        layerwise_learning_rate_decay=0.8)
     trainer = Trainer(
         model=model,
         args=training_args,
         train_dataset=train_ds,
         eval_dataset=val_ds,
         compute_metrics=compute_f1,
-        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)]
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
+        optimizers=(optimizer, scheduler)
     )
     trainer.train()
 
